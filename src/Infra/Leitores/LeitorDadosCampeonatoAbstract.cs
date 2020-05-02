b5c878095cdfc2e65e9f91be738410ef89810e5c
Rafael CP <rafael_it@hotmail.com>
2020-05-02 13:05:57 -0300

    refact: rename class

---
+++ src/Infra/Leitores/LeitorDadosCampeonatoAbstract.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TeamEye.Core.Entities;
+using TeamEye.Core.Interfaces;
+
+namespace TeamEye.Infra.Leitores
+{
+    public abstract class LeitorDadosCampeonatoAbstract : ILeitorDadosCampeonato
+    {
+        public abstract DetalhesRodada InterpretarDetalhesRodada(string linha);
+    }
+}
