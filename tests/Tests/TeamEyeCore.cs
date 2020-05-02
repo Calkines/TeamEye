74204a5bad8178113bf86b4eb913374322544b1e
Rafael CP <rafael_it@hotmail.com>
2020-05-02 13:32:43 -0300

    test: add UnitTest

--- tests/Tests/TeamEyeCore.cs
+++ tests/Tests/TeamEyeCore.cs
@@ -1,14 +1,25 @@
 using System;
+using System.Text;
+using TeamEye.Core.Entities;
+using TeamEye.Infra.Leitores;
 using Xunit;
 
 namespace TeamEye.Tests
 {
     public class TeamEyeCore
     {
-        [Fact]
-        public void Dado_UmaPartidaComVencedor_Quando_RegistrarVitoria_Entao_VencedorDeveReceberTresPontos()
+        [Theory]
+        [InlineData("01	Corinthians		SP		81	38	24	9	5	71	31	")]
+        public void Dado_UmaLinhaEmUmArquivoDeTexto_Quando_RealizarLeituraDaLinha_Entao_UmObjetoRepresentandoOsDetalhesDaRodadaDeveSerRetornado(string linha)
         {
+            //Arrange
+            var leitor = new LeitorTxtDadosCampeonato();
 
+            //Act
+            DetalhesRodada resultado = leitor.InterpretarDetalhesRodada(linha);
+
+            //Assert
+            Assert.NotNull(resultado);
         }
     }
 }
