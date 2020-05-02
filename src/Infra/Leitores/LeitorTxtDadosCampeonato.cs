fcab1b720d9670458129cc6e3747d354938b4d17
Rafael CP <rafael_it@hotmail.com>
2020-05-02 13:34:36 -0300

    feat: includes new reader

---
+++ src/Infra/Leitores/LeitorTxtDadosCampeonato.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TeamEye.Core.Entities;
+
+namespace TeamEye.Infra.Leitores
+{
+    public class LeitorTxtDadosCampeonato : LeitorDadosCampeonatoAbstract
+    {
+        public override DetalhesRodada InterpretarDetalhesRodada(string linha)
+        {
+            var teste = DecompoemLinhasTextoEmPedacos(linha);
+            throw new NotImplementedException();
+        }
+
+        private IEnumerable<string> DecompoemLinhasTextoEmPedacos(string line)
+        {
+            foreach (var chunk in line.Split('\t'))
+            {
+                if(!string.IsNullOrWhiteSpace(chunk))
+                    yield return chunk;
+            }
+        }
+
+        private DetalhesRodada ConvertePedacosTextoEmDetalhesRodada(IEnumerable<string> pedacosTexto)
+        {
+            Dictionary<string, int> relacaoCampoPosicao = new Dictionary<string, int>();
+            
+        }
+    }
+}
