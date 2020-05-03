0c04c00c5d23c9bfbeccccab7c4bd4f6542cc013
Rafael CP <rafael_it@hotmail.com>
2020-05-02 22:01:21 -0300

    refact: add references, remove viewmodel, and add exception

---
+++ src/Infra/Leitores/Exceptions/CabecalhoNaoEncontradoException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamEye.Infra.Leitores.Exceptions
+{
+    public class CabecalhoNaoEncontradoException : Exception
+    {
+        public CabecalhoNaoEncontradoException() : base() { }
+        public CabecalhoNaoEncontradoException(string message) : base(message) { }
+    }
+}
