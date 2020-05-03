86a6ab0e3cdffbec9263a09d250ecf7859aa4d0a
Rafael CP <rafael_it@hotmail.com>
2020-05-02 22:02:01 -0300

    refact: moves from crosscutting and add extesions core specific

---
+++ src/Core/Attributes/TxtDataSourceAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamEye.Core.Attributes
+{
+    public class TxtDataSourceAttribute : Attribute
+    {
+        public int PositionOrder { get; set; }
+    }
+}
