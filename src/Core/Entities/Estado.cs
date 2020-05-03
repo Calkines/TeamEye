7c1a9169370b7b351a4a81a9ff6306a64a38f192
Rafael CP <rafael_it@hotmail.com>
2020-05-02 21:56:05 -0300

    feat: add ctor

--- src/Core/Entities/Estado.cs
+++ src/Core/Entities/Estado.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using TeamEye.Core.Crosscutting.Attributes;
+using TeamEye.Core.Attributes;
 
 namespace TeamEye.Core.Entities
 {
     public class Estado
     {
+        public Estado(string sigla)
+        {
+            Sigla = sigla;
+        }
         [TxtDataSource(PositionOrder = 3)]
         public string Sigla { get; private set; }
     }
